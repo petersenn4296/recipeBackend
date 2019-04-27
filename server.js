dbd5ff8c6dc607e7ff0a3334fff764c59baba5c3
Peter S <petersenn4296@gmail.com>
2019-04-26 21:51:48 -0600

    testing cors

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ let bodyParser = require('body-parser');
 
 let recipe = require('./routes/recipe');
 
-app.get('/', function(req, res, next) {
+let corsOptions = {
+  origin: 'https://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', cors(corsOptions), function(req, res, next) {
     res.send('hello and welcome to alyssa and peters super ultimate recipes backend')
 })
 
@@ -14,7 +19,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-app.use('/recipe', recipe)
+app.use('/recipe', cors(corsOptions), recipe)
 
 app.listen(port, function(){
     console.log(`listening on port ${port}`)
